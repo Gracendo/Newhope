9c4921c6a9f89ea77c22ac964f3eb060414173bb
Gracendo <gracendo96@google.com>
2025-07-20 02:35:12 +0200

    Only display the orphanages who have been created by approved

--- app/Http/Controllers/OrphanageController.php
+++ app/Http/Controllers/OrphanageController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Orphanage;
+use App\Models\Admin;
 
 class OrphanageController extends Controller
 {
     public function index()
     {
-        $orphanages = Orphanage::all();
-
+        // $orphanages = Orphanage::all();
+        // Get only orphanages created by approved admins
+        $orphanages = Orphanage::whereHas('admin', function($query) {
+            $query->where('status', 'approved');
+        })->get();
         return view('frontend.orphanages', compact('orphanages'));
     }
 }
